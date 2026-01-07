622a2330683c64cf45c0792352988b2e7031e004
jul3x <jprolejko@gmail.com>
2026-01-07 08:50:40 +0100

    Add TODOs and fix some code

--- huffmanV3.py
+++ huffmanV3.py
@@ -5,77 +5,61 @@ from bitarray import bitarray
 
 
 class Node:
-    def __init__(self,value,name):
+    def __init__(self, value, name):
         self.name = name
         self.value = value
         self.child_left = None
         self.child_right = None
         
-
     def add_child_left(self,child):
         self.child_left = child
         
-    
     def add_child_right(self,child):
         self.child_right = child
         
-        
-    def delete_child_left(self):
-        self.child_left = None
-        
-        
-    def delete_child_right(self):
-        self.child_right = None
-        
-        
     def __repr__(self):
-        #return f"{self.value}, left child: {self.child_left}, right child: {self.child_right}"
         return f"{self.value,self.name}"
     
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("-f", "--file", required=True, help="directory of the file to compress")
-    parser.add_argument("-b", "--binary", default="results.bin", help="zipped output file")
+    parser.add_argument("-f", "--file", required=True, help="File to compress")
+    parser.add_argument("-b", "--binary", default="results.bin", help="Zipped output file")
     return parser.parse_args()
     
 
-def concordance(doc):
-    if type(doc) != str:
-        return "not text"
+def concordance(doc: str):
     con = dict()
     for letter in doc:
-        if letter not in con:
-            con[letter] = 1
-        else: 
-            con[letter] += 1
+        con[letter] = con.setdefault(letter, 0) + 1
             
     return con
 
-
-def mark(node, current_code=""):
-    global codes
-    global nodes
+# TODO: Do poprawki
+# Tutaj robisz markowanie za pomoca '0' i '1' - kazdy taki string zajmuje 1 bajt
+# Jezeli mamy duzo znakow - to kazdy znak bedzie zajmowal kilka bajtow w mappingu
+# A powinny to byc nie '0', '1' tylko 0b0 i 0b1 - to sa zapisy liczby binarnej
+# Przyklad: jezeli kod to 0000101 to powinienes miec zapis: 5
+def mark(node, codes, nodes, current_code=""):
     if node.child_left:
         codes[node.child_left.name] = current_code + '0'
-        mark(nodes[node.child_left.name], current_code + '0')
-        
+        mark(nodes[node.child_left.name], codes, nodes, current_code + '0')
         
     if node.child_right:
         codes[node.child_right.name] = current_code + '1'
-        mark(nodes[node.child_right.name], current_code + '1')
+        mark(nodes[node.child_right.name], codes, nodes, current_code + '1')
     
 
 def huffman(con):
-    global nodes
-    global codes
+    nodes = {}
+    codes = {}
+
     queue = []
     for i in con.keys():
         queue.append([con[i],i])
     heapq.heapify(queue)
     
     while len(queue) > 1:
-        
         a = heapq.heappop(queue)
         b = heapq.heappop(queue)
         a_node = Node(a[0],a[1])
@@ -85,6 +69,7 @@ def huffman(con):
         new_node = Node(new_val, new_name)
         new_node.add_child_right(a_node)
         new_node.add_child_left(b_node)
+
         if a_node.name not in nodes.keys():
             nodes.update({
                 a_node.name: a_node
@@ -104,33 +89,33 @@ def huffman(con):
         codes.update({
             i: ''
         })
-    mark(root)
+    mark(root, codes, nodes)
     
-    return nodes
+    return nodes, codes
 
 if __name__ == "__main__":
-    
     args = parse_args()
     
-    
-    with open(args.file,'r', encoding="utf-8") as file:
-        dane = file.read()
-
+    with open(args.file, 'r', encoding="utf-8") as f:
+        dane = f.read()
 
     con = concordance(dane)
-    #print(con)
     result = ''
-    codes = {}
-    nodes = dict()
 
-    huffman(con)
+    nodes, codes = huffman(con)
 
+    # TODO: Tutaj mamy problem
+    # Co jesli chcialbys zkompresowac plik ktory ma 100GB?
+    # Program padnie na braku pamieci.
+    # Jakie jest rozwiazanie na to?
+    # Segmentami ladowac plik i miec tylko czesc naraz w pamieci, zbudowac tablice czestosci wystepowania znakow
+    # Zbudowac tak tablice Huffmana i nastepnie segmentami zapisywac do pliku wyjsciowego zakodowane linijki (linijka po linijce).
     for d in dane:
         result += codes[d]
-    #print(result)
 
     b = bitarray(result)  
     
+    # TODO: Format binarny zeby to nie bylo takie wielkie
     codes_leaves = {}
     for key in codes.keys():
         if len(key) == 1:
@@ -138,7 +123,6 @@ if __name__ == "__main__":
                 key: codes[key] 
             })
                       
-
     with open(args.binary, 'wb') as f:
         header = json.dumps(codes_leaves).encode('utf-8')
         f.write(len(header).to_bytes(4, 'big'))  
--- huffman_decoder.py
+++ huffman_decoder.py
@@ -4,36 +4,28 @@ import argparse
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("-f", "--file", required=True, help="directory of the file to decompress")
-    parser.add_argument("-d", "--decoded", default="decoded.txt", help="decompressed file")
+    parser.add_argument("-f", "--file", required=True, help="File to decompress")
+    parser.add_argument("-d", "--decoded", default="decoded.txt", help="Decompressed file")
     return parser.parse_args()
 
 
 if __name__ == "__main__":
-    
     args = parse_args()
     b = bitarray()
     with open(args.file,'rb') as res:
         header_len = int.from_bytes(res.read(4), "big")
         header = res.read(header_len)
         codes = json.loads(header.decode("utf-8"))
-        
         bit_len = int.from_bytes(res.read(4), "big")
-        
         b.fromfile(res)
 
-
-        
     reverse_codes = dict()
     for key in codes.keys():
         reverse_codes.update({
             codes[key]: key
         }) 
         
-    #print(reverse_codes)
-        
     data = b.to01()
-    #print(data)
     result = ''
     current = ''
     for i in range(bit_len):
@@ -42,9 +34,6 @@ if __name__ == "__main__":
             result += reverse_codes[current]
             current = ''
 
-    #print(result)
-
     with open(args.decoded, 'w') as f:
         f.write(result)
-
 
